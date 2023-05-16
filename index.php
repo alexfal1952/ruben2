3158212946fc471edcd78f379479bb4a64c14cbc
alexfal1952 <alexluisopazo@gmail.com>
2023-05-15 22:53:30 -0400

    primer commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+    <?php 
+        include 'head.php';
+    ?>
+</head>
+    <body>
+    <?php 
+        include 'navegacion.php';
+    ?>
+        <div>
+           
+              <div>
+        </div>
+    </body>
+</html>
